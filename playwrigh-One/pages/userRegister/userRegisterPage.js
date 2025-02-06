e9ea4ebc361f1cf139efa14b6d3d8bc58a5c0268
Nelly Mejias <nelly.mejias@applydigital.com>
2025-02-06 17:09:45 -0300

    Actualización clase y objeto

--- playwrigh-One/pages/userRegister/userRegisterPage.js
+++ playwrigh-One/pages/userRegister/userRegisterPage.js
@@ -1,15 +1,16 @@
+import { selectors } from "playwright-core";
+
 export class HomePage {
-    constructor(page) {
-      this.page = page;
-      this.loginButton = 'g[clip-path="url(#clip0_440_247)"]';
+    constructor (page){
+        this.page = page 
+        this.loginButton = 'g[clip-path="url(#clip0_440_247)"]';
     }
-  
+
     async goToHome() {
-      await this.page.goto("https://demo.spreecommerce.org"); 
+        await this.page.goto("https://demo.spreecommerce.org");
     }
-  
+
     async clickLogin() {
-      await this.page.click(this.loginButton)
+        await this.page.click(this.loginButton)
     }
-  }
-  
+}
--- playwrigh-One/tests/userRegister.spec.js
+++ playwrigh-One/tests/userRegister.spec.js
@@ -2,9 +2,9 @@ import { HomePage } from "../pages/userRegister/userRegisterPage";
 import { test } from "@playwright/test";
 test.describe("Login",()=>{
     test('Open the home page', async ({ page }) => {
-        const home = new HomePage (page)
-        await home.goToHome()
-        await home.clickLogin()
+        const homePage = new HomePage (page)
+        await homePage.goToHome()
+        await homePage.clickLogin()
     });
 })
 
